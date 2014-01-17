e65cb34b3f0b5f1e364319c2332fb2efae52631b
Børre Gaup <albbas@gmail.com>
2014-01-17 12:24:43 +0000

    License info

--- corpustools/ccat.py
+++ corpustools/ccat.py
@@ -1,5 +1,25 @@
 # -*- coding:utf-8 -*-
 
+#
+#   This file contains a class and main function to convert giellatekno xml
+#   formatted files to pure text
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright 2013-2014 Børre Gaup <borre.gaup@uit.no>
+#
+
 import inspect
 def lineno():
     """Returns the current line number in our program."""
