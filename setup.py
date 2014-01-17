4b3fcdb61003c0c71b433e0f35c40f0f08f34f42
Børre Gaup <albbas@gmail.com>
2014-01-17 12:25:56 +0000

    Install both ccat and analyse_corpus

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='CorpusTools',
     version='0.1',
     author='Børre Gaup',
     author_email='borre.gaup@uit.no',
-    packages=['corpustools', 'corpustools.test'],
+    packages=find_packages(),
     url='http://divvun.no',
     license='GPL v3.0',
     long_description=open('README.txt').read(),
     entry_points = {
-        'console_scripts': ['ccat = corpustools.ccat:main']
+        'console_scripts': ['ccat = corpustools.ccat:main',
+                            'analyse_corpus = corpustools.analyser:main']
     },
     install_requires=[
         "pyth",
