25d6395fed5598d4b1ad28def2f380f4b8088b3e
Børre Gaup <albbas@gmail.com>
2014-03-05 13:35:25 +0000

    Bump up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CorpusTools',
-    version='0.3.4',
+    version='0.3.5',
     author='Børre Gaup',
     author_email='borre.gaup@uit.no',
     packages=find_packages(),
