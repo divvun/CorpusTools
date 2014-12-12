6bff4695234067e72bdd13e0987530fa2447ed37
Kevin Unhammer <unhammer@fsfe.org>
2014-12-12 12:11:28 +0000

    bruk git://github.com/unhammer/pytidylib.git@giellatekno
    ser ut til å løysa problemet med null-output frå tidy

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     },
     install_requires=[
         "pyth",
-        "pytidylib",
+        #"pytidylib", # in requirements.txt for now
         "beautifulsoup4",
         "unidecode",
         "lxml",
