ff127ab0887b6e0c764a518be2155a71d94c7a81
Børre Gaup <albbas@gmail.com>
2014-12-19 17:16:26 +0000

    And try to add the new dir to the vcs working copy at the same time

--- corpustools/namechanger.py
+++ corpustools/namechanger.py
@@ -85,6 +85,7 @@ class AddFileToCorpus(NameChangerBase):
     def makedirs(self):
         try:
             os.makedirs(self.new_dirname)
+            self.vcs.add(self.new_dirname)
         except OSError:
             pass
 
