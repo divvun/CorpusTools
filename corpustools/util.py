57a8d08490a82c8b50fba75753ba34c8544ebd5f
Børre Gaup <albbas@gmail.com>
2014-12-30 13:16:43 +0000

    flake8 conformance

--- corpustools/util.py
+++ corpustools/util.py
@@ -1,20 +1,24 @@
 import os
-import lxml.etree as etree
+
 
 class SetupException(Exception):
     pass
 
+
 class ExecutableMissingException(Exception):
     pass
 
+
 def is_executable(fullpath):
     return os.path.isfile(fullpath) and os.access(fullpath, os.X_OK)
+
 
 def path_possibilities(program):
     return (os.path.join(path.strip('"'),
                          program)
             for path
             in os.environ["PATH"].split(os.pathsep))
+
 
 def executable_in_path(program):
     fpath, fname = os.path.split(program)
@@ -25,13 +29,14 @@ def executable_in_path(program):
                    for possible_path in
                    path_possibilities(program))
 
+
 def sanity_check(program_list):
     u"""Look for programs and files that are needed to do the analysis.
     If they don't exist, raise an exception.
     """
-    if not 'GTHOME' in os.environ:
-        raise SetupException("You have to set the environment variable GTHOME to "
-                             "your checkout of langtech/trunk!")
+    if 'GTHOME' not in os.environ:
+        raise SetupException("You have to set the environment variable GTHOME "
+                             "to your checkout of langtech/trunk!")
     for program in program_list:
         if executable_in_path(program) is False:
             raise ExecutableMissingException(
