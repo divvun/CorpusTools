b06fc03f1948fae5e1d14a93c4d1df8661d756a4
Kevin Unhammer <unhammer@fsfe.org>
2015-02-20 08:31:27 +0000

    more text_cat tests

--- corpustools/test/test_text_cat.py
+++ corpustools/test/test_text_cat.py
@@ -16,4 +16,24 @@ class TestTextCat(unittest.TestCase):
         self.assertEqual(
             LANGUAGEGUESSER.classify("eg køyrer ikkje"),
             "nno")
+        self.assertEqual(
+            LANGUAGEGUESSER.classify("Sámediggi nammada sámi báikenammakonsuleanttaid"),
+            "sme")
 
+    def test_folder_none(self):
+        fullguesser = text_cat.Classifier(None)
+        self.assertEqual(
+            fullguesser.classify("eg køyrer ikkje"),
+            "nno")
+        self.assertEqual(
+            fullguesser.classify("Almmá norggabeale guohtuneatnamiid ii leat vejolaš jođihit"),
+            "sme")
+
+    def test_restricted(self):
+        littleguesser = text_cat.Classifier(langs=["nob","sma"])
+        self.assertEqual(
+            littleguesser.classify("Regional utvikling"),
+            "nob")
+        self.assertEqual(
+            littleguesser.classify("Regijovnaale evtiedimmie"),
+            "sma")
