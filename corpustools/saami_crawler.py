a62a22aab87e2c2b5ef5f870a08245ac6969a720
Børre Gaup <albbas@gmail.com>
2016-02-24 16:39:19 +0000

    Unwanted links and page content

--- corpustools/saami_crawler.py
+++ corpustools/saami_crawler.py
@@ -155,7 +155,8 @@ class SamediggiFiCrawler(object):
         '''
         return ('ei ole saatavilla' in content or
                 'There are no translations available' in content or
-                '<div class="login-form">' in content)
+                '<div class="login-form">' in content or
+                'Sinulla ei ole tarvittavia' in content)
 
     def harvest_links(self, content):
         '''Harvest all links, bar some restrictions
@@ -192,7 +193,7 @@ class SamediggiFiCrawler(object):
                             'klemetti.blogspot|/nuorat|/#|com_events|'
                             'com_search|haettavana|do_pdf|pop=1|com_docman|'
                             '/images|com_weblink|task=vcard|view_contact_id|'
-                            'com_contact|mad4joomla',
+                            'com_contact|mad4joomla|mailto',
                             href) and
                         href.startswith('http://www.samediggi.fi')):
                     self.unvisited_links.add(href)
