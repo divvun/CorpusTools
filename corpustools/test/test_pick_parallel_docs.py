c1f6c4c99074aa53acb31f6272daf8844efc30a4
Børre Gaup <albbas@gmail.com>
2016-12-15 12:03:29 +0000

    Make tests work again

--- corpustools/test/test_pick_parallel_docs.py
+++ corpustools/test/test_pick_parallel_docs.py
@@ -14,7 +14,8 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this file. If not, see <http://www.gnu.org/licenses/>.
 #
-#   Copyright © 2012-2016 The University of Tromsø & the Norwegian Sámi Parliament
+#   Copyright © 2012-2016 The University of Tromsø &
+#                        the Norwegian Sámi Parliament
 #   http://giellatekno.uit.no & http://divvun.no
 #
 
@@ -23,61 +24,85 @@ from __future__ import absolute_import, print_function
 import os
 import unittest
 
+import git
+import testfixtures
+
 from corpustools import parallelize, pick_parallel_docs
 
-here = os.path.dirname(__file__)
+HERE = os.path.dirname(__file__)
 
+
+article47_sme = u'''<?xml version="1.0" encoding="UTF-8"?>
+<document id="no_id" xml:lang="sme">
+  <header>
+    <translated_from xml:lang="nob"/>
+    <wordcount>209</wordcount>
+    <parallel_text location="article-47.html" xml:lang="nob"/>
+  </header>
+</document>
+'''
+
+article47_nob = u'''<?xml version="1.0" encoding="UTF-8"?>
+<document id="no_id" xml:lang="nob">
+  <header>
+    <wordcount>209</wordcount>
+    <parallel_text location="article-47.html" xml:lang="sme"/>
+  </header>
+</document>
+'''
 
 class TestParallelPicker(unittest.TestCase):
 
     def setUp(self):
-        self.language1_converted_dir = os.path.join(
-            here, 'pick_parallel_docs/converted/sme/admin')
+        self.tempdir = testfixtures.TempDirectory(ignore=['.git'])
+        r = git.Repo.init(self.tempdir.path)
+        self.tempdir.makedir('converted/sme/admin')
+        self.tempdir.makedir('converted/nob/admin')
 
-        self.pp = pick_parallel_docs.ParallelPicker(
+        self.language1_converted_dir = os.path.join(
+            self.tempdir.path, 'converted/sme/admin')
+
+        self.tempdir.write('converted/sme/admin/article-47.html.xml',
+                           article47_sme.encode('utf8'))
+        self.tempdir.write('converted/nob/admin/article-47.html.xml',
+                           article47_nob.encode('utf8'))
+        self.picker = pick_parallel_docs.ParallelPicker(
             self.language1_converted_dir, 'nob', '73', '110')
 
+    def tearDown(self):
+        self.tempdir.cleanup()
+
     def test_calculate_language1(self):
-        self.pp.calculate_language1(self.language1_converted_dir)
-        self.assertEqual(self.pp.get_language1(), 'sme')
+        self.picker.calculate_language1('converted/sme/admin')
+        self.assertEqual(self.picker.language1, 'sme')
 
     def test_get_parallel_language(self):
-        self.assertEqual(self.pp.get_parallel_language(), 'nob')
+        self.assertEqual(self.picker.parallel_language, 'nob')
 
-    def test_has_orig(self):
-        file_with_orig1 = parallelize.CorpusXMLFile(os.path.join(
-            self.language1_converted_dir, 'samediggi-article-47.html.xml'))
-        self.assertEqual(self.pp.has_orig(file_with_orig1), True)
-
-        language1_prestable_converted_dir = os.path.join(
-            here, 'pick_parallel_docs/prestable/converted/sme/admin')
-        file_with_orig2 = parallelize.CorpusXMLFile(os.path.join(
-            language1_prestable_converted_dir, 'samediggi-article-47.html.xml'))
-        self.assertEqual(self.pp.has_orig(file_with_orig2), True)
-
-        file_without_orig1 = parallelize.CorpusXMLFile(os.path.join(
-            self.language1_converted_dir, 'samediggi-article-1.html.xml'))
-        self.assertEqual(self.pp.has_orig(file_without_orig1), False)
-
-        file_without_orig2 = parallelize.CorpusXMLFile(os.path.join(
-            language1_prestable_converted_dir, 'samediggi-article-1.html.xml'))
-        self.assertEqual(self.pp.has_orig(file_without_orig2), False)
-
-    def test_has_parallel(self):
+    def test_has_parallel1(self):
+        """Parallel exists, parallel_text points to correct place."""
         file_with_parallel1 = parallelize.CorpusXMLFile(os.path.join(
-            self.language1_converted_dir, 'samediggi-article-47.html.xml'))
-        self.assertEqual(self.pp.has_parallel(file_with_parallel1), True)
+            self.language1_converted_dir,
+            'article-47.html.xml'))
+        self.assertEqual(self.picker.has_parallel(file_with_parallel1),
+                         True)
 
-        language1_prestable_converted_dir = os.path.join(
-            here, 'pick_parallel_docs/prestable/converted/sme/admin')
-        file_with_parallel2 = parallelize.CorpusXMLFile(os.path.join(
-            language1_prestable_converted_dir, 'samediggi-article-47.html.xml'))
-        self.assertEqual(self.pp.has_parallel(file_with_parallel2), True)
+    def test_has_parallel2(self):
+        """parallel_text points to wrong language."""
+        file_with_parallel1 = parallelize.CorpusXMLFile(os.path.join(
+            self.language1_converted_dir,
+            'article-47.html.xml'))
+        file_with_parallel1.etree.find('//parallel_text').set(
+            '{http://www.w3.org/XML/1998/namespace}lang', 'sma')
+        self.assertEqual(self.picker.has_parallel(file_with_parallel1),
+                         False)
 
-        file_without_parallel1 = parallelize.CorpusXMLFile(os.path.join(
-            self.language1_converted_dir, 'samediggi-article-53.html.xml'))
-        self.assertEqual(self.pp.has_parallel(file_without_parallel1), False)
-
-        file_without_parallel2 = parallelize.CorpusXMLFile(os.path.join(
-            language1_prestable_converted_dir, 'samediggi-article-53.html.xml'))
-        self.assertEqual(self.pp.has_parallel(file_without_parallel2), False)
+    def test_has_parallel3(self):
+        """parallel_text points to wrong file."""
+        file_with_parallel1 = parallelize.CorpusXMLFile(os.path.join(
+            self.language1_converted_dir,
+            'article-47.html.xml'))
+        file_with_parallel1.etree.find('//parallel_text').set(
+            'location', 'article-48.html')
+        self.assertEqual(self.picker.has_parallel(file_with_parallel1),
+                         False)
