51cd6f02ebee43a6f53d5cfc1265bcc495f0be17
Børre Gaup <albbas@gmail.com>
2017-01-12 20:40:21 +0000

    New year

--- corpustools/modes.py
+++ corpustools/modes.py
@@ -14,7 +14,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this file. If not, see <http://www.gnu.org/licenses/>.
 #
-#   Copyright © 2013-2016 The University of Tromsø &
+#   Copyright © 2013-2017 The University of Tromsø &
 #                         the Norwegian Sámi Parliament
 #   http://giellatekno.uit.no & http://divvun.no
 #
