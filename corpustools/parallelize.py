37500b4546928a2932f4988f46f88a5f2f3e0113
Børre Gaup <albbas@gmail.com>
2017-03-16 14:24:30 +0000

    In our fst world, nno == nob

--- corpustools/parallelize.py
+++ corpustools/parallelize.py
@@ -86,7 +86,7 @@ class SentenceDivider(object):
         Arguments:
             lang (str): language the analyser can analyse
         """
-        self.lang = lang
+        self.lang = 'nob' if lang == 'nno' else lang
         self.relative_path = relative_path
 
     def setup_pipeline(self):
