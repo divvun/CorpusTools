e3d4cfd4e8f02cb092872c24ae85ce22df4e7b85
Børre Gaup <albbas@gmail.com>
2017-05-10 10:50:23 +0000

    Move AvvirConverter to a separate file

---
+++ corpustools/avvirconverter.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright © 2012-2017 The University of Tromsø &
+#                         the Norwegian Sámi Parliament
+#   http://giellatekno.uit.no & http://divvun.no
+#
+
+u"""Convert Ávvir-files to the giellatekno xml format."""
+
+
+from lxml import etree
+
+
+class AvvirConverter(object):
+    u"""Convert Ávvir xml files to the giellatekno xml format.
+
+    The root node in an Ávvir document is article.
+    article nodes contains one or more story nodes.
+    story nodes contain one or more p nodes.
+    p nodes contain span, br and (since 2013) p nodes.
+    """
+
+    def __init__(self, intermediate):
+        """Initialise the AvvirConverter class.
+
+        Arguments:
+            filename: string containing the path to the file that should
+            be converted
+        """
+        self.intermediate = intermediate
+
+    def remove_identical_ids(self):
+        """Remove identical ids."""
+        story_ids = set()
+        for story in self.intermediate.xpath('.//story[@id]'):
+            story_id = story.get('id')
+            if story_id not in story_ids:
+                story_ids.add(story_id)
+            else:
+                story.getparent().remove(story)
+
+    @staticmethod
+    def insert_element(para, text, position):
+        """Insert a new element in p's parent.
+
+        Arguments:
+            p: an lxml element, it is a story/p element
+            text: (unicode) string
+            position: (integer) the position inside p's parent where the new
+                      element is inserted
+
+        Returns:
+            position: (integer)
+        """
+        if text is not None and text.strip() != '':
+            new_p = etree.Element('p')
+            new_p.text = text
+            grandparent = para.getparent()
+            grandparent.insert(grandparent.index(para) + position, new_p)
+            position += 1
+
+        return position
+
+    @staticmethod
+    def convert_sub_p(para):
+        """Convert p element found inside story/p elements.
+
+        These elements contain erroneous text that an editor has removed.
+        This function removes p.text and saves p.tail
+
+        Arguments:
+            p: an lxml element, it is a story/p element
+        """
+        for sub_p in para.findall('.//p'):
+            previous = sub_p.getprevious()
+            if previous is None:
+                parent = sub_p.getparent()
+                if sub_p.tail is not None:
+                    if parent.text is not None:
+                        parent.text = parent.text + sub_p.tail
+                    else:
+                        parent.text = sub_p.tail
+            else:
+                if sub_p.tail is not None:
+                    if previous.tail is not None:
+                        previous.tail = previous.tail + sub_p.tail
+                    else:
+                        previous.tail = sub_p.tail
+            para.remove(sub_p)
+
+    def convert_subelement(self, para):
+        """Convert subelements of story/p elements to p elements.
+
+        Arguments:
+            p: an lxml element, it is a story/p element
+        """
+        position = 1
+        for subelement in para:
+            position = self.insert_element(para, subelement.text, position)
+
+            for subsubelement in subelement:
+                for text in [subsubelement.text, subsubelement.tail]:
+                    position = self.insert_element(para, text, position)
+
+            position = self.insert_element(para, subelement.tail, position)
+
+            para.remove(subelement)
+
+    def convert_p(self):
+        """Convert story/p elements to one or more p elements."""
+        for para in self.intermediate.findall('./story/p'):
+            if para.get("class") is not None:
+                del para.attrib["class"]
+
+            self.convert_sub_p(para)
+            self.convert_subelement(para)
+
+            if para.text is None or para.text.strip() == '':
+                story = para.getparent()
+                story.remove(para)
+
+    def convert_story(self):
+        """Convert story elements in to giellatekno xml elements."""
+        for title in self.intermediate.findall('.//story[@class="Tittel"]'):
+            for para in title.findall('./p'):
+                para.set('type', 'title')
+
+            del title.attrib['class']
+            del title.attrib['id']
+
+            title.tag = 'section'
+
+        for title in self.intermediate.findall(
+                './/story[@class="Undertittel"]'):
+            for para in title.findall('./p'):
+                para.set('type', 'title')
+
+            del title.attrib['class']
+            del title.attrib['id']
+
+            title.tag = 'section'
+
+        for story in self.intermediate.findall('./story'):
+            parent = story.getparent()
+            for i, para in enumerate(story.findall('./p')):
+                parent.insert(parent.index(story) + i + 1, para)
+
+            parent.remove(story)
+
+    def convert_article(self):
+        u"""The root element of an Ávvir doc is article, rename it to body."""
+        self.intermediate.tag = 'body'
+        document = etree.Element('document')
+        document.append(self.intermediate)
+        self.intermediate = document
+
+
+def convert2intermediate(filename):
+    u"""Convert an Ávvir xml to an intermediate xml document."""
+    avvir = AvvirConverter(etree.parse(filename))
+    avvir.remove_identical_ids()
+    avvir.convert_p()
+    avvir.convert_story()
+    avvir.convert_article()
+
+    return avvir.intermediate
---
+++ corpustools/test/test_avvirconverter.py
@@ -0,0 +1,327 @@
+# -*- coding: utf-8 -*-
+
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright © 2014-2017 The University of Tromsø &
+#                         the Norwegian Sámi Parliament
+#   http://giellatekno.uit.no & http://divvun.no
+#
+
+u"""Test conversion of Ávvir xml files."""
+
+import lxml.etree as etree
+
+from corpustools import avvirconverter
+from corpustools.test import xmltester
+
+
+class TestAvvirConverter(xmltester.XMLTester):
+    """Test the functionality of the Avvir class."""
+
+    def setUp(self):
+        """Setup a common AvvirConverter instance."""
+        self.avvir = avvirconverter.AvvirConverter(etree.fromstring(
+            '<article>'
+            '    <story id="a" class="Tittel">'
+            '        <p>a</p>'
+            '    </story>'
+            '    <story id="b" class="Undertittel">'
+            '        <p>b</p>'
+            '    </story>'
+            '    <story id="c" class="ingress">'
+            '        <p>c</p>'
+            '    </story>'
+            '    <story id="d" class="body">'
+            '        <p class="tekst">d<br/>e<br/></p>'
+            '        <p>f</p>'
+            '    </story>'
+            '    <story id="g" class="body">'
+            '        <p class="tekst">h<span>i</span>j</p>'
+            '    </story>'
+            '    <story id="k" class="body">'
+            '        <p>l'
+            '            <span>'
+            '                m'
+            '                <br/>'
+            '                n'
+            '            </span>'
+            '            o'
+            '        </p>'
+            '    </story>'
+            '    <story id="a">'
+            '        <p>a<p>b</p></p>'
+            '    </story>'
+            '</article>'
+        ))
+
+    def test_remove_identical_ids(self):
+        """Check that only the first of stories with identical ids are kept."""
+        want = etree.fromstring(
+            '<article>'
+            '    <story id="a" class="Tittel">'
+            '        <p>a</p>'
+            '    </story>'
+            '    <story id="b" class="Undertittel">'
+            '        <p>b</p>'
+            '    </story>'
+            '    <story id="c" class="ingress">'
+            '        <p>c</p>'
+            '    </story>'
+            '    <story id="d" class="body">'
+            '        <p class="tekst">d<br/>e<br/></p>'
+            '        <p>f</p>'
+            '    </story>'
+            '    <story id="g" class="body">'
+            '        <p class="tekst">h<span>i</span>j</p>'
+            '    </story>'
+            '    <story id="k" class="body">'
+            '        <p>l'
+            '            <span>'
+            '                m'
+            '                <br/>'
+            '                n'
+            '            </span>'
+            '            o'
+            '        </p>'
+            '    </story>'
+            '</article>'
+        )
+
+        self.avvir.remove_identical_ids()
+        self.assertXmlEqual(self.avvir.intermediate, want)
+
+    def test_convert_p_1(self):
+        """Check when p does not contain p."""
+        want = etree.fromstring(
+            '<article>'
+            '    <story class="Tittel" id="a">'
+            '        <p>a</p>'
+            '    </story>'
+            '    <story class="Undertittel" id="b">'
+            '        <p>b</p>'
+            '    </story>'
+            '    <story class="ingress" id="c">'
+            '        <p>c</p>'
+            '    </story>'
+            '    <story class="body" id="d">'
+            '        <p>d</p>'
+            '        <p>e</p>'
+            '        <p>f</p>'
+            '    </story>'
+            '    <story class="body" id="g">'
+            '        <p>h</p>'
+            '        <p>i</p>'
+            '        <p>j</p>'
+            '    </story>'
+            '    <story class="body" id="k">'
+            '        <p>l</p>'
+            '        <p>m</p>'
+            '        <p>n</p>'
+            '        <p>o</p>'
+            '    </story>'
+            '</article>')
+
+        self.avvir.remove_identical_ids()
+        self.avvir.convert_p()
+        self.assertXmlEqual(self.avvir.intermediate, want)
+
+    def test_convert_p_2(self):
+        """Check when p contains only p."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>corrected text <p>text with typo</p>with tail</p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>corrected text with tail</p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_p_3(self):
+        """Check when p contains span and p."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>'
+            '           <span>bla bla</span>'
+            '           corrected text <p>text with typo</p>with tail'
+            '       </p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>bla bla</p>'
+            '       <p>corrected text with tail</p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_p_4(self):
+        """p.text is None."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p><p> </p>with tail'
+            '       </p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p> with tail</p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_p_5(self):
+        """sub_p.tail is None."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>láigovistti <p class="NormalParagraphStyle">85</p>'
+            '       </p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>láigovistti </p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_p_6(self):
+        """previous.text not None, sub_p.tail is None."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p class="Privat ann tittel">Stohpu<br/>vuovdemassi'
+            '<p class="NormalParagraphStyle">85</p><br/></p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>Stohpu</p>'
+            '       <p>vuovdemassi</p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_p_7(self):
+        """previous.tail is None, sub_p.tail not None."""
+        avvir = avvirconverter.AvvirConverter('orig/sme/admin/fakename.xml')
+        avvir.intermediate = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p class="Privat ann tittel">'
+            '<br/><p class="NormalParagraphStyle">157</p>Ozan visttáža <br/>'
+            '       </p>'
+            '   </story>'
+            '</article>')
+
+        want = etree.fromstring(
+            '<article>'
+            '   <story class="body">'
+            '       <p>Ozan visttáža</p>'
+            '   </story>'
+            '</article>')
+
+        avvir.convert_p()
+        self.assertXmlEqual(avvir.intermediate, want)
+
+    def test_convert_story(self):
+        """Test convert_story."""
+        want = etree.fromstring(
+            '<article>'
+            '    <section>'
+            '        <p type="title">a</p>'
+            '    </section>'
+            '    <section>'
+            '        <p type="title">b</p>'
+            '    </section>'
+            '    <p>c</p>'
+            '    <p>d</p>'
+            '    <p>e</p>'
+            '    <p>f</p>'
+            '    <p>h</p>'
+            '    <p>i</p>'
+            '    <p>j</p>'
+            '    <p>l</p>'
+            '    <p>m</p>'
+            '    <p>n</p>'
+            '    <p>o</p>'
+            '</article>')
+
+        self.avvir.remove_identical_ids()
+        self.avvir.convert_p()
+        self.avvir.convert_story()
+        self.assertXmlEqual(self.avvir.intermediate, want)
+
+    def test_convert_article(self):
+        """Test convert_article."""
+        want = etree.fromstring(
+            '<document>'
+            '    <body>'
+            '        <section>'
+            '            <p type="title">a</p>'
+            '        </section>'
+            '        <section>'
+            '            <p type="title">b</p>'
+            '        </section>'
+            '        <p>c</p>'
+            '        <p>d</p>'
+            '        <p>e</p>'
+            '        <p>f</p>'
+            '        <p>h</p>'
+            '        <p>i</p>'
+            '        <p>j</p>'
+            '        <p>l</p>'
+            '        <p>m</p>'
+            '        <p>n</p>'
+            '        <p>o</p>'
+            '    </body>'
+            '</document>')
+
+        self.avvir.remove_identical_ids()
+        self.avvir.convert_p()
+        self.avvir.convert_story()
+        self.avvir.convert_article()
+        self.assertXmlEqual(self.avvir.intermediate, want)
