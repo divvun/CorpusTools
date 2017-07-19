3958ad051cf04baa813653d151a57e58c78bc2ed
Børre Gaup <albbas@gmail.com>
2017-07-19 09:55:54 +0000

    Use better variable names

--- corpustools/test/test_decode.py
+++ corpustools/test/test_decode.py
@@ -142,9 +142,9 @@ class TestEncodingGuesser(unittest.TestCase):
             test_input[u'winsami2_to_cp1252'][0]), want)
 
     def test_meadowmari_cp1252(self):
-        uffperted = u'ОЙСАВЫШ 139 В.ЕГОРОВ. Романыште ҥ Ҥ ӱ Ӱ ӧ Ӧ ӱ Ӱ ӧ Ӧ'
-        perverted = u'ÎÉÑÀÂÛØ 139 Â.ÅÃÎÐÎÂ. Ðîìàíûøòå ‰ ˆ ¢ ™ º ª ў Ў є Є'
-        self.assertEqual(decode.fix_meadowmari_cp1252(perverted), uffperted)
+        correct = u'ОЙСАВЫШ 139 В.ЕГОРОВ. Романыште ҥ Ҥ ӱ Ӱ ӧ Ӧ ӱ Ӱ ӧ Ӧ'
+        uncorrect = u'ÎÉÑÀÂÛØ 139 Â.ÅÃÎÐÎÂ. Ðîìàíûøòå ‰ ˆ ¢ ™ º ª ў Ў є Є'
+        self.assertEqual(decode.fix_meadowmari_cp1252(uncorrect), correct)
 
     def test_macsami_macroman(self):
         uff = u'Ω'
