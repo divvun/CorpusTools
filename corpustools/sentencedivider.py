88b61bc97e5763809e7490e2ec612e20fa63da81
Børre Gaup <albbas@gmail.com>
2018-01-23 11:22:59 +0000

    Move the training corpus functionality to separate files

--- corpustools/sentencedivider.py
+++ corpustools/sentencedivider.py
@@ -24,15 +24,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import io
 import os
-import sys
 
 import regex
 from lxml import etree
-from shutil import copy
 
-from corpustools import ccat, modes, util
-
-HERE = os.path.dirname(__file__)
+from corpustools import ccat, modes
 
 
 def to_plain_text(lang, filename):
@@ -155,135 +151,3 @@ class SentenceDivider(object):
                 valid_sentences.append(sentence)
 
         return valid_sentences
-
-
-class TrainingCorpusMaker(object):
-    """Turn analysed giella xml files into training corpus.
-
-    Filter out all sentences containing words unknown to the
-    giella fst analysers.
-
-    Attributes:
-        only_words: regex catching word made up of letters.
-        xml_printer (ccat.XMLPrinter): extracts the dependency analysis
-            from the giella xml files.
-    """
-
-    only_words = regex.compile(r'\p{L}+')
-    xml_printer = ccat.XMLPrinter(dependency=True)
-
-    def parse_dependency(self, text):
-        """Parse the dependency element found in a giella xml file.
-
-        Arguments:
-            text (str): contains the dependency element of a giella xml file.
-
-        Yields:
-            str: a sentence containing only words known to the giella fst
-                analysers, that contain at least a word as identified by
-                the only_words regex.
-        """
-        sentence_buffer = []
-        uff_buffer = []
-        for line in io.StringIO(text):
-            line = line.rstrip()
-            if line == ':' or line == ':\\n':
-                sentence_buffer.append(' ')
-            elif line.startswith(':'):
-                uff_buffer.append(line)
-            elif line.startswith('"'):
-                sentence_buffer.append(line[2:-2])
-            elif 'CLB' in line:
-                if not ('".' not in line and '"¶"' not in line
-                        and '"?"' not in line and '"!"' not in line
-                        and '"…"' not in line):
-                    if uff_buffer:
-                        for uff in uff_buffer:
-                            util.print_frame(uff)
-                    else:
-                        sentence_line = ''.join(sentence_buffer).replace(
-                            '¶', '').strip()
-                        if self.only_words.search(sentence_line):
-                            yield sentence_line
-                    uff_buffer[:] = []
-                    sentence_buffer[:] = []
-            elif '" ?' in line:
-                uff_buffer.append(line)
-
-    def file_to_sentences(self, filename):
-        """Turn a giella xml into a list of sentences.
-
-        Arguments:
-            filename (str): name of the giella xml file containing a
-                dependency element.
-
-        Returns:
-            list of str
-        """
-        self.xml_printer.parse_file(filename)
-        text = self.xml_printer.process_file().getvalue()
-        if text.strip():
-            return [
-                sentence for sentence in self.parse_dependency(text)
-                if sentence
-            ]
-        else:
-            return []
-
-    def analysed_files(self):
-        """Find analysed files.
-
-        Yields:
-            str: filename of an analysed file.
-        """
-        for corpus in [
-                os.path.join(os.getenv('GTFREE'), 'analysed', sys.argv[1]),
-                os.path.join(os.getenv('GTBOUND'), 'analysed', sys.argv[1])
-        ]:
-            for root, _, files in os.walk(corpus):
-                for file_ in files:
-                    yield (os.path.join(root, file_))
-
-    def make_corpus_files(self):
-        """Make .txt files from .xml files.
-
-        The .txt files contain only sentences with words known to the
-        giella fsts.
-        """
-        for analysed_file in self.analysed_files():
-            if analysed_file.endswith('.xml'):
-                with open(analysed_file.replace('.xml', '.txt'), 'w') as txt_stream:
-                    txt_stream.write('\n'.join(
-                        self.file_to_sentences(analysed_file)))
-                    txt_stream.write('\n')
-
-    def pytextcat_corpus(self):
-        """Turn the free and bound corpus into a pytextcat training corpus."""
-        corpus_dir = os.path.join('pytextcat', sys.argv[1])
-        with util.ignored(OSError):
-            os.makedirs(corpus_dir)
-
-        with open('{}.txt'.format(os.path.join(corpus_dir, sys.argv[1])),
-                  'w') as corpusfile:
-            for analysed_file in self.analysed_files():
-                if analysed_file.endswith('.txt'):
-                    with open(analysed_file) as analysed:
-                        corpusfile.write(analysed.read())
-
-    def langid_corpus(self):
-        """Turn the free and bound corpus into a langid training corpus."""
-        for analysed_file in self.analysed_files():
-            if analysed_file.endswith('.txt'):
-                langid_dir = 'langid/{}/{}'.format(
-                    util.split_path(analysed_file).genre, sys.argv[1])
-                with util.ignored(OSError):
-                    os.makedirs(langid_dir)
-                copy(analysed_file, langid_dir)
-
-
-def main():
-    """Turn the corpus into a pytextcat training corpus."""
-    sentence_maker = TrainingCorpusMaker()
-    sentence_maker.make_corpus_files()
-    sentence_maker.pytextcat_corpus()
-    sentence_maker.langid_corpus()
---
+++ corpustools/test/test_trainingcorpusmaker.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright © 2018 The University of Tromsø &
+#                    the Norwegian Sámi Parliament
+#   http://divvun.no & http://giellatekno.uit.no
+#
+"""Test sentence division functionality."""
+from __future__ import absolute_import, print_function, unicode_literals
+
+import unittest
+
+from corpustools import trainingcorpusmaker
+
+
+class TestTrainingCorpusMaker(unittest.TestCase):
+    """Test the TrainingCorpusMaker class.
+
+    Attributes:
+        sentencemaker (corpustools.TrainingCorpusMaker)
+    """
+
+    def setUp(self):
+        """Set up the TrainingCorpusMaker."""
+        self.sentencemaker = trainingcorpusmaker.TrainingCorpusMaker()
+
+    def test_no_unknown(self):
+        """Test with only known input."""
+        test_input = '\n'.join([
+            '"<Oahpa>"',
+            '\t"Oahpa" N Prop Sem/Obj Sg Nom <W:0.0000000000> @HNOUN #1->0',
+            '"<:>"',
+            '\t":" CLB <W:0.0000000000> #2->1',
+            ': ',
+            '"<Soahki>"',
+            '\t"soahki" N Sem/Plant Sg Nom <W:0.0000000000> @HNOUN #3->1',
+            ': ',
+            '"<¶>"',
+            '\t"¶" CLB <W:0.0000000000> #4->1',
+            ':\n',
+            '"<addit>"',
+            '\t"addit" V TV Inf <W:0.0000000000> @FS-N<IMV #13->11',
+            ': ',
+            '"<boahtte>"',
+            '\t"boahtte" A Sem/Dummytag Attr <W:0.0000000000> @>N #14->15',
+            ': ',
+            '"<bulvii>"',
+            '\t"buolva" N Sem/Body_Group_Hum_Time Sg Ill <W:0.0000000000> @<ADVL #15->13',
+            '"<.>"',
+            '\t"." CLB <W:0.0000000000> #16->4',
+            ': ',
+            '',
+            '"<¶>"',
+            '\t"¶" CLB <W:0.0000000000> #1->1',
+            ':\n',
+            '',
+        ])
+
+        want = 'Oahpa: Soahki\naddit boahtte bulvii.'
+        got = '\n'.join([
+            sentence
+            for sentence in self.sentencemaker.parse_dependency(test_input)
+            if sentence
+        ])
+        self.assertEqual(got, want)
+
+    def test_with_comma(self):
+        """Check that comma is handled correctly."""
+        test_input = '\n'.join([
+            '"<áhkuin>"',
+            '\t"áhkku" N Sem/Hum Sg Com <W:0.0000000000> @<ADVL #6->1',
+            '"<,>"',
+            '\t"," CLB <W:0.0000000000> #7->6',
+            ': ',
+            '"<ádjáin>"',
+            '\t"áddjá" N Sem/Hum Sg Com <W:0.0000000000> @<ADVL #8->1',
+            ': ',
+            '"<dahje>"',
+            '\t"dahje" CC <W:0.0000000000> @CNP #9->8',
+            ': ',
+            '"<earáin>"',
+            '\t"eará" Pron Indef Sg Com <W:0.0000000000> @<ADVL #10->1',
+            '"<!>"',
+            '\t"!" CLB <W:0.0000000000> #13->1',
+            ': ',
+            '',
+            '"<¶>"',
+            '\t"¶" CLB <W:0.0000000000> #1->1',
+            ':\n',
+        ])
+
+        want = 'áhkuin, ádjáin dahje earáin!'
+        got = '\n'.join([
+            sentence
+            for sentence in self.sentencemaker.parse_dependency(test_input)
+            if sentence
+        ])
+        self.assertEqual(got, want)
