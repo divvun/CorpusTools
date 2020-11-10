4d64b2f9b9dcccc75ea6f805404e9f4bc84a990a
Børre Gaup <albbas@gmail.com>
2020-11-10 12:51:21 +0000

    Add mrj among languages deemed worthy of decoding detection

--- corpustools/converter.py
+++ corpustools/converter.py
@@ -212,7 +212,7 @@ class Converter(object):
 
         if (self.metadata.get_variable('mainlang') in [
                 'sma', 'sme', 'smj', 'smn', 'sms', 'nob', 'fin', 'swe', 'nno',
-                'dan', 'fkv', 'sju', 'sje', 'mhr'
+                'dan', 'fkv', 'sju', 'sje', 'mhr', 'mrj'
         ]):
             try:
                 fixer.fix_body_encoding(self.metadata.get_variable('mainlang'))
