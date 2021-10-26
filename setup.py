6a99a107b43116832d2a527be980171f6b8f75f3
Børre Gaup <albbas@gmail.com>
2021-10-26 11:02:19 +0200

    Fix yet another typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "Børre Gaup <borre.gaup@uit.no>, "
         "Kevin Unhammer, "
         "Ciprian Gerstenberger, "
-        "Chiara Argese <chiara.argese@iut.no>"
+        "Chiara Argese <chiara.argese@uit.no>"
     ),
     packages=find_packages(),
     url="http://github.com/giellalt/CorpusTools",
