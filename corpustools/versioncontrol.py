e78d078634b4ba9079e2365a5eaa30085e2a8994
Børre Gaup <albbas@gmail.com>
2022-03-30 18:47:11 +0200

    Remove dependency on pysvn
    From now on, check out corpus directories using `git svn clone`.

--- corpustools/versioncontrol.py
+++ corpustools/versioncontrol.py
@@ -24,7 +24,6 @@ import os
 import pwd
 
 import git
-import pysvn
 
 
 class VersionControlError(Exception):
@@ -72,91 +71,6 @@ class VersionController:
             return self.config.get("user", "email")
         else:
             return ""
-
-
-class SVN(VersionController):
-    """Implement basic svn functionality."""
-
-    def __init__(self, svnclient):
-        """Initialise the SVN class.
-
-        Args:
-            svnclient (pysvn.Client): an svn client to control the
-                working copy.
-        """
-        super().__init__()
-        self.client = svnclient
-
-    def add_path(self, path):
-        """Add a single path to the working copy.
-
-        Args:
-            path (str): path to a file or directory
-        """
-        valid_path = self.valid_svn_path(path)
-        self.client.add(valid_path, recurse=True, force=True)
-        if valid_path.endswith(".xsl"):
-            self.client.propset("svn:mime-type", "text/xml", valid_path)
-
-    def valid_svn_path(self, path):
-        """Find the part of the path that is under version control.
-
-        Args:
-            path (str): path that should be added to the working copy
-
-        Returns:
-            str: path to the first unversioned part of the path
-        """
-        child = os.path.basename(path)
-        parent = os.path.dirname(path)
-        while not self.under_version_control(parent):
-            child = os.path.basename(parent)
-            parent = os.path.dirname(parent)
-
-        return os.path.join(parent, child)
-
-    def under_version_control(self, path):
-        """Check if path is under version control.
-
-        Args:
-            path (str): path that should be checked.
-
-        Returns:
-            bool: True if under version control, False otherwise
-        """
-        try:
-            status = self.client.status(path)[0].text_status
-        except pysvn.ClientError:
-            return False
-        else:
-            return status not in (
-                pysvn.wc_status_kind.added,
-                pysvn.wc_status_kind.unversioned,
-                pysvn.wc_status_kind.ignored,
-            )
-
-    def add(self, path):
-        """Add path to the working copy.
-
-        Args:
-            path (str or list of str): path may be a list of paths or a
-                single path
-        """
-        if isinstance(path, list):
-            for p in path:
-                self.add_path(p)
-        else:
-            self.add_path(path)
-
-    def move(self, oldpath, newpath):
-        """Move file in the working copy."""
-        if self.client.info(oldpath) is not None:
-            self.client.move(oldpath, newpath)
-
-    def remove(self, path):
-        """Remove a file from the working copy."""
-        if self.client.info(path) is not None:
-            self.client.remove(path)
 
 
 class GIT(VersionController):
@@ -208,16 +122,9 @@ def vcs(directory):
         Either the SVN or the GIT class.
     """
     try:
-        svn_client = pysvn.Client()
-        svn_client.info(directory)
-        return SVN(svn_client)
-    except pysvn.ClientError:
-        try:
-            git_repo = git.Repo(directory)
-            return GIT(git_repo)
-        except git.exc.InvalidGitRepositoryError:
-            raise VersionControlError(
-                "{} is neither a SVN working repository or a Git repo. "
-                "Files can only be added to a version controlled "
-                "directory.".format(directory)
-            )
+        git_repo = git.Repo(directory)
+        return GIT(git_repo)
+    except git.exc.InvalidGitRepositoryError:
+        raise VersionControlError(
+            f"{directory} is not a Git repo. Files can only be added to a git repo."
+        )
