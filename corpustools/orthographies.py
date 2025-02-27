ea0fdcfd587076de8f3a0b1af6046d6f9eb60d7c
Anders Lorentsen <anders.lorentsen@gmail.com>
2025-02-27 18:15:33 +0100

    'standard' is always a valid orthography

--- corpustools/orthographies.py
+++ corpustools/orthographies.py
@@ -18,4 +18,8 @@ def orthographies(want_only_lang=None):
 
 
 def is_orthography_of(ortho, lang):
+    # the special name "standard" is always a valid orthography,
+    # regardless of which lang we're asking about.
+    if ortho == "standard":
+        return True
     return ortho in ORTHOGRAPHIES[lang]
