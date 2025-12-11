d8b153a6f6b534ff49c9e49be669f7e824644196
Børre Gaup <albbas@gmail.com>
2025-12-11 10:36:40 +0100

    Copyright and info headers

--- corpustools/error_markup.py
+++ corpustools/error_markup.py
@@ -1,3 +1,20 @@
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright © 2025 The University of Tromsø &
+#                         the Norwegian Sámi Parliament
+#   http://giellatekno.uit.no & http://divvun.no
+#
 """Error markup representation for grammar checking.
 
 Represents marked up errors in sentences with support for nested errors.
@@ -7,8 +24,6 @@ This indicates an error "čohke" with correction "čohkke" of type "errorortreal
 
 Example nested markup: {{epoxi}${noun,cons|epoksy} lim}¢{noun,mix|epoksylim}
 This indicates a nested error where "epoxi" has its own correction within a larger error.
-
-Ported from divvun-runtime/cli/src/command/yaml_test/error_markup.rs
 """
 
 from __future__ import annotations
--- corpustools/test/test_error_markup.py
+++ corpustools/test/test_error_markup.py
@@ -1,3 +1,20 @@
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright © 2025 The University of Tromsø &
+#                         the Norwegian Sámi Parliament
+#   http://giellatekno.uit.no & http://divvun.no
+#
 """Tests for error_markup module, ported from Rust tests."""
 
 from corpustools.error_annotated_sentence import ErrorAnnotatedSentence
